d3b1738d8156869e6c73ca7ebecd22204f0b39c1
Ahmed Adam <ahmad.adam@extendedfamily.leading-point.com>
2024-08-21 14:00:07 +0300

    create a package

---
+++ BuildFormExample/BuildFormExample/BuildFormExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildFormExampleApp.swift
+//  BuildFormExample
+//
+//  Created by Ahmad on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BuildFormExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
